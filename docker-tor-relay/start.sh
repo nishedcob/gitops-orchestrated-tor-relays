37a903758d16bccfacbc8db5ac8702d9e57a153b
Nicholas Spalding <nspalding@ioet.com>
2019-03-05 15:53:51 -0500

    Tor Relay Dockerization, runtime improvements

--- docker-tor-relay/start.sh
+++ docker-tor-relay/start.sh
@@ -11,12 +11,16 @@ function config_injection() {
   return $(eval "$CMD")
 }
 
-config_injection "\(Nickname \)NAME_ME_PLEASE" "\1$RELAY_NICKNAME/"
+config_injection "\(Nickname \)NAME_ME_PLEASE" "\1$RELAY_NICKNAME"
 config_injection "\(ORPort \)TOR_PORT" "\1$TOR_PORT"
 config_injection "\(ExitRelay \)EXIT_RELAY" "\1$EXIT_RELAY"
 config_injection "\(SocksPort \)SOCKS_PORT" "\1$SOCKS_PORT"
 config_injection "\(ControlSocket \)CONTROL_SOCKET" "\1$CONTROL_SOCKET"
 config_injection "\(ContactInfo \)CONTACT_INFO" "\1$CONTACT_INFO"
+config_injection "\(MaxMemInQueues \)MAX_MEMORY" "\1$MAX_MEMORY"
+
+echo "$TOR_CONFIG_FILE:"
+cat $TOR_CONFIG_FILE
 
 # Pulled from /lib/system/tor@default.service:
 TOR_BINARY="/usr/bin/tor"
